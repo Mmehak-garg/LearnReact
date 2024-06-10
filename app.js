a24399d287e3546418df5bceed20f841694de6e1
Mehak <mmehak_be21@thapar.edu>
2024-06-10 10:23:22 +0530

    day_01

---
+++ app.js
@@ -0,0 +1,26 @@
+{/* <div id:"parent">
+    <div id:"child">
+        <h1></h1>
+        <h1></h1>
+    </div>
+</div> */}
+
+const parent = React.createElement("div",
+    {id:"parent"},
+        [React.createElement(
+            "div",
+            {id:"child"},
+           [ React.createElement("h1",{}, "hello"),
+            React.createElement("h2",{}, "hello")]
+        ),React.createElement(
+            "div",
+            {id:"child2"},
+           [ React.createElement("h1",{}, "hello"),
+            React.createElement("h2",{}, "hello")]
+        )]
+);
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+ const root = ReactDOM.createRoot(document.querySelector("#root"));    
+// root.render(heading);
+root.render(parent);
